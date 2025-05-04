7362985b91b7833e0e107b48517059d5a49be87f
user <naifalhumaidi@proton.me>
2025-05-04 03:00:48 +0300

    Create loading.tsx. complete the dashboard main page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,5 @@
 import { Nav, NavLink } from "@/components/Nav";
-
+export const dynamic = "force-dynamic";
 export default function AdminLayout({
     children,
   }: Readonly<{
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,20 +4,21 @@ import db from "@/lib/prisma";
 
 
 export default async function Dashboard() {
-  // const {amount, NumberOfSales} = await getOrdersData();
-  const {amount,NumberOfSales} = await getOrdersData();
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(), getUserData(), getProductData()
+  ])
+  
   return (
     <>
     <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      <DashboardCard title={"Sales"} subtitle={formatNumber(NumberOfSales)} content={formatCurrency(amount)}/>
-      <DashboardCard title={"title"} subtitle={"sub"} content={"content"}/>
-      <DashboardCard title={"title"} subtitle={"sub"} content={"content"}/>
+      <DashboardCard title={"Sales"} subtitle={formatNumber(salesData.NumberOfSales)} content={formatCurrency(salesData.amount)}/>
+      <DashboardCard title={"Customers"} subtitle={formatNumber(userData.userCount)} content={formatCurrency(userData.averageValuePerUser)}/>
+      <DashboardCard title={"Product"} subtitle={formatNumber(productData.activeUsers)} content={formatNumber(productData.inactiveUsers)}/>
     </div>
     </>
   );
 }
-
-const getOrdersData = async () => {
+const getSalesData = async () => {
   const data = await db.order.aggregate(
     {
       _sum: { pricePaidInCents:true },
@@ -28,6 +29,31 @@ const getOrdersData = async () => {
      amount:(data._sum.pricePaidInCents || 0) / 100,
      NumberOfSales:data._count
    }
+}
+
+const getUserData = async () => {
+  const [userCount,orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate(
+      {_sum:{pricePaidInCents:true}}
+    ) 
+  ])
+  return {
+    userCount,
+    averageValuePerUser: userCount === 0 ? 0: 
+    (orderData._sum.pricePaidInCents || 0) / userCount / 100
+  }
+}
+
+const getProductData = async () => {
+  const [activeUsers, inactiveUsers] = await Promise.all([
+    db.product.count({where:{isAvailable:true}}), 
+    db.product.count({where:{isAvailable:false}}), 
+  ])
+  return {
+    activeUsers,
+    inactiveUsers
+  };
 }
 
 type DashboardCardProps = {title:string, subtitle:string, content:string}
