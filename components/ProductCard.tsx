2e454a2751f3b6ffb7872aea79a733ed9b287783
user <naifalhumaidi@proton.me>
2025-06-12 23:59:00 +0300

    Complete the productCard component

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -2,43 +2,46 @@ import { formatCurrency } from "@/lib/formatter";
 import { Product } from "@/generated/prisma";
 import Image from "next/image";
 import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardContent,
-    CardDescription,
-    CardFooter,
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+  CardFooter,
 } from "@/components/ui/card";
 import { Button } from "./ui/button";
 import Link from "next/link";
 
-const ProductCard = ({ id, name, description, priceInCents }: Product ) => {
-    return (
-      <Card key={id} className="flex flex-col overflow-hidden">
-        <CardHeader>
-          <Image
-            src="/products/Screenshot_20250602_101242.png"
-            //!
-            // src={product.imagePath}
-            alt="Product Image"
-            width={400}
-            height={400}
-          />
-          <CardTitle>
-            <h3 className="text-2xl">{name}</h3>
-            <p>{formatCurrency(priceInCents / 100)}</p>
-          </CardTitle>
-        </CardHeader>
-  
-        <CardContent>
-          <CardDescription>{description}</CardDescription>
-        </CardContent>
+const ProductCard = ({ id, name, description, priceInCents, imagePath }: Product) => {
+  return (
+    <Card key={id} className="flex flex-col overflow-hidden pt-0">
+      <CardHeader className="relative h-auto w-full aspect-video">
+        <Image
+          fill
+          src="/products/Screenshot_20250602_101242.png"
+          //!
+          // src={imagePath}
+          alt={name}
+        />
+        <CardTitle>
+          <h3 className="text-2xl">{name}</h3>
+          <p>{formatCurrency(priceInCents / 100)}</p>
+        </CardTitle>
+      </CardHeader>
 
-        <CardFooter>
-          <Button className="w-full py-6"><Link href={`/products/${id}/purchase`}>Purchase</Link></Button>
-        </CardFooter>
-      </Card>
-    );
-  };
-  
-  export default ProductCard;
+      <CardContent className="flex-grow">
+        <CardDescription>
+          <p className="line-clamp-3">{description}</p>
+        </CardDescription>
+      </CardContent>
+
+      <CardFooter>
+        <Button className="w-full py-6">
+          <Link href={`/products/${id}/purchase`}>Purchase</Link>
+        </Button> {/* //? asChild doesn't work here */}
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ProductCard;
